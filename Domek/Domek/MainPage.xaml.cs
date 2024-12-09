3d38997057d5d8809b7aebdccc38a1b9008c295e
DESKTOP-5CTFEFT\mateusz <matimichalski01@gmail.com>
2024-12-09 08:52:00 +0100

    aplikacja mobilna

---
+++ Domek/Domek/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace Domek
+{
+    public partial class MainPage : ContentPage
+    {
+
+        int polubieniaCount = 1;
+        public MainPage()
+        {
+            InitializeComponent();
+           
+
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            polubieniaCount++;
+            polubienia.Text = $"{polubieniaCount} Polubienia";
+
+
+        }
+        private void Delete(object sender, EventArgs e)
+        {
+            
+            if (polubieniaCount > 1)
+            {
+                polubieniaCount--;
+                polubienia.Text = $"{polubieniaCount} Polubienia";
+            }
+
+
+        }
+    }
+
+}
